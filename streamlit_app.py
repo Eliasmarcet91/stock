755b4d869f7e3cb205619d1c53d64f9b19e59495
Eliasmarcet91 <73188124+Eliasmarcet91@users.noreply.github.com>
2024-03-23 10:26:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.title('🎈 App Name')
+# Define a function to load the model and apply the st.cache decorator
+@st.cache(allow_output_mutation=True)
+def load_model():
+    with open('stock_prediction(2).pkl', 'rb') as file:
+        model = pickle.load(file)
+    return model
 
-st.write('Hello world!')
+# Load the pickled model using the cached function
+model = load_model()
+
+# Main application title
+st.title('Stock Time Series and Sentiment Analysis')
+
+# User input for word or sentence
+user_input = st.text_input("Enter a word or sentence from the movie American Psycho to get its vector:", "")
